f0764206f2099581bb4f3c359c7a0bed8fddf8b4
Moktadir Rahman <autocircled@gmail.com>
2020-06-28 10:23:38 +0600

    modified

---
+++ assets/js/admin-script.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function($){
+
+    
+
+    var myOptions = {
+        // you can declare a default color here,
+        // or in the data-default-color attribute on the input
+        defaultColor: '#002fff',
+        // a callback to fire whenever the color changes to a valid color
+        change: function(event, ui){},
+        // a callback to fire when the input is emptied or an invalid color
+        clear: function() {
+            alert('Empty/invalid color');
+        },
+        // hide the color picker controls on load
+        hide: true,
+        // show a group of common colors beneath the square
+        // or, supply an array of colors to customize further
+        palettes: true
+    };
+    $('.autocircle-color-field').wpColorPicker(myOptions);
+    
+
+    $("#wqpmb-enable-quantity-button").change(function() {
+        $('.section-button-cunsomize').toggleClass('show', this.checked);
+    }).change();
+});
