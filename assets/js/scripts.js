2b6bdb09be9c2beccb0e6f578e322a1a9546842c
Saiful Islam <codersaiful@gmail.com>
2021-08-08 11:34:24 +0600

    max step handle

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -19,32 +19,55 @@ jQuery(function ($) {
             var max = parseFloat(qty.attr("max"));
             var min = parseFloat(qty.attr("min"));
             var step = parseFloat(qty.attr("step"));
+            var static = 8;
+            
+            var qtyObj = $(this).siblings(".quantity");
+            var last_step = qtyObj.attr('data-last_step');
 
             // Change input field value if result is in min and max range.
             // If the result is above max then change to max and alert user about exceeding max stock.
             // If the field is empty, fill with min for "-" (0 possible) and step for "+".
             if ($(this).is(".plus")) {
-                if (val === max)
+                if (val === max){
+                    console.log(555);
                     return false;
+                }
+                    
                 if (isNaN(val)) {
+                    console.log(666);
                     qty.val(step);
                     return false;
                 }
                 if (val + step > max) {
+                    qtyObj.attr('data-last_step', ( max-val ) );
                     qty.val(max);
                 } else {
+                    console.log(888);
                     qty.val(val + step);
                 }
             } else {
-                if (val === min)
+
+                if (last_step) {
+                    
+                    qty.val(max - static);
+                    qtyObj.removeAttr('data-last_step');
                     return false;
+                }
+                if (val === min){
+                    console.log(1111);
+                    return false;
+                }
+                    
                 if (isNaN(val)) {
+                    console.log(2222);
                     qty.val(min);
                     return false;
                 }
                 if (val - step < min) {
+                    console.log(3333);
                     qty.val(min);
                 } else {
+                    console.log(4444);
                     qty.val(val - step);
                 }
             }
