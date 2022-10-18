959a181ab90ab44e8f86ef07fdda6d48b436eebe
Saiful Islam <codersaiful@gmail.com>
2022-10-18 12:10:05 +0600

    linkedin Added

--- includes/admin-menu.php
+++ includes/admin-menu.php
@@ -263,6 +263,7 @@ function wqpmb_social_links(){
     $codeastrology = [
         'web'   => ['url' => 'https://codeastrology.com/?utm=Plugin_Social', 'title' => 'CodeAstrology'],
         'wpt'   => ['url' => 'https://wooproducttable.com/?utm=Plugin_Social', 'title' => 'Woo Product Table'],
+        'linkedin'   => ['url' => 'https://www.linkedin.com/company/codeastrology'],
         'youtube'   => ['url' => 'https://www.youtube.com/c/codeastrology'],
         'facebook'   => ['url' => 'https://www.facebook.com/codeAstrology'],
         'twitter'   => ['url' => 'https://www.twitter.com/codeAstrology'],
