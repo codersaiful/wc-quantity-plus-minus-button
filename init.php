99db506f8c8b565dafd96cfa5d9aa8a8f346d2c5
Saiful Islam <codersaiful@gmail.com>
2023-08-16 09:44:12 +0600

    WC tested up to: 	 8.0.2

--- init.php
+++ init.php
@@ -13,7 +13,7 @@
  * Requires at least:    4.0.0
  * Tested up to:         6.3
  * WC requires at least: 3.7
- * WC tested up to: 	 8.0.1
+ * WC tested up to: 	 8.0.2
  */
 if ( ! defined( 'ABSPATH' ) ) {
     die();
